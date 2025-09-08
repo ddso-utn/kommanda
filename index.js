ce56b3a8961f7a95cf388fd4d60bd6f9a6e657dd
Rodolfo Leandro Caputo <rodcap.93@gmail.com>
2025-09-08 18:32:10 -0300

    wip

--- index.js
+++ index.js
@@ -15,21 +15,8 @@ app.post('/platos', PlatosController.crearPlato)
 
 app.get('/platos', PlatosController.verPlatos)
 
-app.put('/platos/:id', PlatosController.actualizarPlato)
-
 app.patch('/platos/:id', PlatosController.marcarPlatoDisponible)
 
-app.post('/comandas', ComandaController.crearComanda)
-
-app.get('/comandas/:id', ComandaController.verComanda)
-
-app.patch('/comandas/:id', ComandaController.actualizarBebidasComanda)
-
-app.post('/comandas/:id/platos', ComandaController.agregarPlatosComanda)
-
-app.patch('/comandas/:id/platos/:ordenPlato', ComandaController.actualizarPlatoComanda)
-
-app.get('/comandas', ComandaController.buscarComanda)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
