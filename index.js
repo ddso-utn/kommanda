94657b7d9e0da025ff2de52884fb552b872ca56c
Rodolfo Leandro Caputo <rodcap.93@gmail.com>
2025-09-08 22:05:35 -0300

    crear comanda [finalziado]

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import bodyParser from "body-parser";
 import {PlatosController} from "./src/controllers/platosController.js";
 import express from 'express'
+import {ComandaController} from "./src/controllers/comandaController.js";
 const app = express()
 const port = 3000
 
@@ -17,6 +18,8 @@ app.get('/platos', PlatosController.verPlatos)
 app.get('/platos/:id', PlatosController.verPlato)
 
 app.put('/platos/:id', PlatosController.actualizarPlato)
+
+app.post('/comandas', ComandaController.crearComanda)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/controllers/comandaController.js
@@ -0,0 +1,31 @@
+import {Comanda, PlatoPedido} from "../domain/dominio.js";
+import {ComandaRepository} from "../repositories/comandaRepository.js";
+import {Menu} from "../repositories/menu.js";
+import {ComandaInvalida} from "../excepciones/comandas.js";
+import {PlatoInexistente, PlatoInvalido} from "../excepciones/platos.js";
+
+export const ComandaController = {
+
+  crearComanda(req, res) {
+    try {
+      const mesa = req.body.mesa;
+      const platosPedidos = req.body.platos.map(p =>
+        new PlatoPedido(
+          Menu.obtenerPlatoPorId(p.idPlato),
+          p.cantidad,
+          p.notas
+        )
+      );
+      const comanda = ComandaRepository.agregarComanda(new Comanda(mesa, platosPedidos))
+      res.status(201).json(comanda)
+    } catch (error) {
+      console.error(error)
+      if (error instanceof ComandaInvalida || error instanceof PlatoInexistente) {
+        res.status(400).json({
+          error: error.message,
+        })
+      }
+    }
+  }
+}
+
