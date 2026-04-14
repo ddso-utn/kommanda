49ee226d107d55dc02a4072da47937da5deac1a3
Rodolfo Leandro Caputo <rodcap.93@gmail.com>
2026-04-14 16:58:36 -0300

    clase-1 paso 5: ver platos
    verPlatos y verPlato implementados en index con manejo de errores.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { readFileSync } from 'fs'
 import { parse } from 'yaml'
 import {Plato} from "./src/domain/plato.js";
 import {Menu} from "./src/repositories/menu.js";
-import {PlatoInvalido} from "./src/excepciones/platos.js";
+import {PlatoInvalido, PlatoInexistente} from "./src/excepciones/platos.js";
 
 const app = express()
 const port = 3000
@@ -30,15 +30,17 @@ app.post('/platos', (req, res) => {
 })
 
 app.get('/platos', (req, res) => {
-  res.status(200).json([
-    { id: 1, nombre: "Milanesa con puré", precio: 10000, categoria: "PRINCIPAL", estaDisponible: true },
-    { id: 2, nombre: "Ensalada César", precio: 7000, categoria: "ENTRADA", estaDisponible: true },
-    { id: 3, nombre: "Flan casero", precio: 4000, categoria: "POSTRE", estaDisponible: false }
-  ])
+  res.status(200).json(Menu.listar())
 })
 
 app.get('/platos/:id', (req, res) => {
-  res.status(200).json({ id: 1, nombre: "Milanesa con puré", precio: 10000, categoria: "PRINCIPAL", estaDisponible: true })
+  try {
+    res.status(200).json(Menu.obtenerPlatoPorId(parseInt(req.params.id)))
+  } catch(error) {
+    if(error instanceof PlatoInexistente) {
+      res.status(404).json({ error: error.message })
+    }
+  }
 })
 
 app.put('/platos/:id', (req, res) => {
