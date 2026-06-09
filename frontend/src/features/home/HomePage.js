bc0d8ebc7323e4d295620253614e78133d0f0a48
Rodolfo Leandro Caputo <rodcap.93@gmail.com>
2026-06-09 16:02:20 -0300

    clase-react paso 1: separando en componentes (Titulo, NavBar, CardPlato, ListaPlatos)

--- frontend/src/features/home/HomePage.js
+++ frontend/src/features/home/HomePage.js
@@ -1,91 +1,121 @@
 import "./home.css"
 
+
+const PLATOS = [
+  {
+    "nombre": "Pizza",
+    "imagen": "https://images.unsplash.com/photo-1601924582971-6e9f8d6d1e4e",
+    "precio": 16000,
+    "disponibleHastaHs": 20
+  },
+  {
+    "nombre": "Empanadas",
+    "imagen": "https://images.unsplash.com/photo-1617196038435-9e4f9f6e8b1e",
+    "precio": 9000,
+    "disponibleHastaHs": 21
+  },
+  {
+    "nombre": "Milanesa con papas",
+    "imagen": "https://images.unsplash.com/photo-1586190848861-99aa4a171e90",
+    "precio": 18000,
+    "disponibleHastaHs": 22
+  },
+  {
+    "nombre": "Ravioles con tuco",
+    "imagen": "https://images.unsplash.com/photo-1603079841834-6e4f9f6e8b1e",
+    "precio": 17000,
+    "disponibleHastaHs": 20
+  },
+  {
+    "nombre": "Hamburguesa completa",
+    "imagen": "https://images.unsplash.com/photo-1550547660-d9450f859349",
+    "precio": 15000,
+    "disponibleHastaHs": 23
+  },
+  {
+    "nombre": "Tarta de verdura",
+    "imagen": "https://images.unsplash.com/photo-1603079841834-6e4f9f6e8b1e",
+    "precio": 12000,
+    "disponibleHastaHs": 19
+  },
+  {
+    "nombre": "Sushi",
+    "imagen": "https://images.unsplash.com/photo-1553621042-f6e147245754",
+    "precio": 22000,
+    "disponibleHastaHs": 22
+  },
+  {
+    "nombre": "Pollo al horno con papas",
+    "imagen": "https://images.unsplash.com/photo-1603079841834-6e4f9f6e8b1e",
+    "precio": 18500,
+    "disponibleHastaHs": 20
+  },
+  {
+    "nombre": "Fideos con salsa",
+    "imagen": "https://images.unsplash.com/photo-1603079841834-6e4f9f6e8b1e",
+    "precio": 14000,
+    "disponibleHastaHs": 21
+  },
+  {
+    "nombre": "Lomo con puré",
+    "imagen": "https://images.unsplash.com/photo-1603079841834-6e4f9f6e8b1e",
+    "precio": 21000,
+    "disponibleHastaHs": 22
+  },
+  {
+    "nombre": "Ensalada César",
+    "imagen": "https://images.unsplash.com/photo-1603079841834-6e4f9f6e8b1e",
+    "precio": 13000,
+    "disponibleHastaHs": 19
+  },
+  {
+    "nombre": "Tacos",
+    "imagen": "https://images.unsplash.com/photo-1603079841834-6e4f9f6e8b1e",
+    "precio": 16000,
+    "disponibleHastaHs": 22
+  }
+]
+
+
+const Titulo = ({texto}) => {
+  return <h1 className="title">{texto}</h1>
+}
+
+const NavBar = ({logo, slogan}) => {
+  return <navbar className="nav">
+    <div className="logo">{logo}</div>
+    <div className="">{slogan}</div>
+  </navbar>
+}
+
+const CardPlato = ({nombre, imagen, precio}) => {
+  return <div className="card">
+    <h3>{nombre}</h3>
+    <img src={imagen}/>
+    <p className="price">${precio}</p>
+  </div>
+}
+
+const ListaPlatos = ({}) => {
+  return <div className="platos">{
+    PLATOS.map((plato) => <CardPlato
+      nombre={plato.nombre}
+      imagen={plato.imagen}
+      precio={plato.precio}
+    />)
+  }</div>
+}
+
 const Home = () => {
-    return (
-      <section className="home">
-        <navbar className="nav">
-          <div className="logo">Kommanda</div>
-          <div className="">El sitio con +50 platos!</div>
-        </navbar>
-        <div className="content">
-          <h1 className="title">Platos</h1>
-          <div className="platos">
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-            <div className="card">
-              <h3>Pizza</h3>
-              <img
-                src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg"/>
-              <p className="price">$16.000</p>
-            </div>
-          </div>
-        </div>
-      </section>
-    )
+  return (
+    <section className="home">
+      <NavBar logo="Kommanda" slogan="El sitio con +50 platos!"/>
+      <div className="content">
+        <Titulo texto="Platos"/>
+        <ListaPlatos/>
+      </div>
+    </section>
+  )
 };
 
 export default Home; 
