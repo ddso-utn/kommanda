ccf05dc7b18ad58d9a99c0e367191534ac1b26cd
Rodolfo Leandro Caputo <rodcap.93@gmail.com>
2026-06-16 16:27:57 -0300

    clase-react paso 13: estado de carga explicito

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,15 +13,18 @@ function App() {
   const [platos, setPlatos] = useState([]);
   const [bebidas, setBebidas] = useState([]);
   const [banner, setBanner] = useState('');
+  const [platosLoading, setPlatosLoading] = useState(false);
   const [comanda, setComanda] = useState({platos: [], bebidas: []});
 
   const cargarPlatos = async () => {
+    setPlatosLoading(true)
     const platos = await axios.get('https://68486e64ec44b9f34940e355.mockapi.io/kommanda/platos-base')
       .then(r => r.data);
     const imagenes = await axios.get('https://68486e64ec44b9f34940e355.mockapi.io/kommanda/images').then(r => r.data);
     setPlatos(platos.map(p => ({...p, imagen: imagenes[0][p.id]})));
     const banner = await axios.get('https://68486e64ec44b9f34940e355.mockapi.io/kommanda/banner').then(r => r.data);
     setBanner(banner[0].message)
+    setPlatosLoading(false)
   }
 
   useEffect(() => {
@@ -48,7 +51,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout cantPlatos={platos.length}/>}>
-          {<Route path="/platos" element={platos.length === 0 ? "Cargando..." : <Platos
+          {<Route path="/platos" element={platosLoading ? "Cargando..." : <Platos
             banner={banner}
             todosLosPlatos={platos}
             alAgregarAComanda={agregarPlatosAComanda}
