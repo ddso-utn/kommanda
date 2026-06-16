2d068e90aa237db20cf198fc744e675ba59b3105
Rodolfo Leandro Caputo <rodcap.93@gmail.com>
2026-06-16 16:24:45 -0300

    clase-react paso 7: agregar notas a los platos

--- frontend/src/features/platos/Platos.js
+++ frontend/src/features/platos/Platos.js
@@ -7,18 +7,24 @@ const Titulo = ({texto}) => {
   return <h1 className="title">{texto}</h1>
 }
 
-const CardPlato = ({nombre, imagen, precio, seleccionado, alSeleccionarPlato}) => {
+const CardPlato = ({nombre, imagen, precio, seleccionado, alSeleccionarPlato, notas, alCambiarNotas}) => {
   return <div
     className={`card ${seleccionado && "selected"}`}
     onClick={alSeleccionarPlato}
   >
     <h3>{nombre}</h3>
     <img src={imagen}/>
+    <input
+      placeholder="Notas..."
+      type={"text"}
+      value={notas || ""}
+      onChange={e => alCambiarNotas(e.target.value)}
+    />
     <p className="price">${precio}</p>
   </div>
 }
 
-const ListaPlatos = ({platos, cambiarSeleccionPlato}) => {
+const ListaPlatos = ({platos, cambiarSeleccionPlato, cambiarNotasPlato}) => {
   return <div className="platos">{
     platos.map((plato) => <CardPlato
       key={plato.id}
@@ -27,6 +33,8 @@ const ListaPlatos = ({platos, cambiarSeleccionPlato}) => {
       precio={plato.precio}
       seleccionado={plato.seleccionado}
       alSeleccionarPlato={() => cambiarSeleccionPlato(plato.id)} // "onDishSelect"
+      notas={plato.notas}
+      alCambiarNotas={notas => cambiarNotasPlato(plato.id,notas)}
     />)
   }</div>
 }
@@ -43,7 +51,7 @@ const Platos = () => {
     platos.filter(p => p.seleccionado)
 
   const agregarAComanda = async () => {
-    console.log("Agregando platos...", platosSeleccionados().map(p => p.nombre))
+    console.log("Agregando platos...", platosSeleccionados().map(p => `${p.nombre} (Notas: ${p.notas})`))
     await putCommanda(platosSeleccionados())
     setPlatos(platos.map(p => ({...p, seleccionado: false})))
     alert("Platos agregados!")
@@ -60,6 +68,15 @@ const Platos = () => {
     platos.map(p => p.id === idPlato ? cambiarSeleccion(p) : p)
   )
 
+  const cambiarNotas = (plato, notas) => {
+    // Devuelve un plato nuevo modificado ... como en Haskell!
+    return {...plato, notas}
+  }
+
+  const cambiarNotasPlato = (idPlato, notas) => setPlatos(
+    platos.map(p => p.id === idPlato ? cambiarNotas(p, notas) : p)
+  )
+
   return (
     platos.length === 0 ? "Cargando..." : <section className="home">
       <div className="content">
@@ -67,6 +84,7 @@ const Platos = () => {
         <ListaPlatos
           platos={platos}
           cambiarSeleccionPlato={cambiarSeleccionPlato}
+          cambiarNotasPlato={cambiarNotasPlato}
         />
       </div>
       <div className="actions">
